210b99d6c6efa223744018e3e2ef783d36b8eb6e
slicky1 <okunonke@gmail.com>
2021-03-28 21:18:43 -0400

    still populating

--- scr/index.js
+++ scr/index.js
@@ -20,3 +20,17 @@ function clickAlert() {
 alert (`Cape Verde or Cabo Verde, officially the Republic of Cabo Verde, is an archipelago and island country in the central Atlantic Ocean, consisting of ten volcanic islands with a combined land area of about 4,033 square kilometres.`);
 }
 capeVerde.addEventListener(`click`, clickAlert);
+
+let gambia = country[3];
+
+function clickAlert() {
+alert (`The Gambia is a small West African country, bounded by Senegal, with a narrow Atlantic coastline. It's known for its diverse ecosystems around the central Gambia River. Abundant wildlife in its Kiang West National Park and Bao Bolong Wetland Reserve includes monkeys, leopards, hippos, hyenas and rare birds. The capital, Banjul, and nearby Serrekunda offer access to beaches.`);
+}
+gambia.addEventListener(`click`, clickAlert);
+
+let ghana = country[4];
+
+function clickAlert() {
+alert (`Ghana, officially the Republic of Ghana which was formerly called Gold Coast, is a country along the Gulf of Guinea and the Atlantic Ocean, in the subregion of West Africa.`);
+}
+ghana.addEventListener(`click`, clickAlert);
