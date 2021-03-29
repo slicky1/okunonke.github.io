f76f54b2bfde22fa3944d428437563f529f8378d
slicky1 <okunonke@gmail.com>
2021-03-28 20:48:17 -0400

    javascripting

--- scr/index.js
+++ scr/index.js
@@ -1,1 +1,8 @@
-alert("Hello");
+const country = document.getElementsByClassName ("country");
+country[0];
+let benin = country[0];
+benin
+function clickAlert() {
+alert (`Benin, a French-speaking West African nation, is a birthplace of the vodun (or “voodoo”) religion and home to the former Dahomey Kingdom from circa 1600–1900. In Abomey, Dahomey's former capital, the Historical Museum occupies two royal palaces with bas-reliefs recounting the kingdom’s past and a throne mounted on human skulls. To the north, Pendjari National Park offers safaris with elephants, hippos and lions.`);
+}
+benin.addEventListener(`click`, clickAlert);
